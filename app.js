1f8c78849acf385d9f00c8daca698b67969facf0
SamBen16 <adresse123456789101112@gmail.com>
2024-06-02 16:37:38 +0200

    creation d'une route Get +ajout middleware CORS

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
+app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+app.get('/', (req, res) => {
+  res.json({ message: 'bienvenue sur la page d accueil' });
 });
 
 app.use((req, res, next) => {
   console.log('Réponse envoyée avec succès !');
+  next();
 });
+
 
 module.exports = app;
